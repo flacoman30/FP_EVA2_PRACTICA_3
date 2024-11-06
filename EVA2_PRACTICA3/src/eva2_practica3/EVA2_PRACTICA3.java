2b568a1d679c1756fa9e5d867762b491ed1cbff0
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-11-06 13:56:23 -0600

    Add files via upload

---
+++ EVA2_PRACTICA3/src/eva2_practica3/EVA2_PRACTICA3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author flac3
+ */
+
+        }
+    }
+}
+
+    
+
+    
+    
+}
